682cf7591d0f1db9329225c99f26a7d33de05758
Andres Maldonado-Martin <andresdmm1@hotmail.com>
2022-03-06 17:37:00 -0500

    Import characters

---
+++ Fighter's Uproar/Assets/UI/MainMenu/MainMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject titleMenu;
+    public TextMeshProUGUI pressStart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
