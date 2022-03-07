87e51fefd7c31a6a91996d8d040c01bf72bf74b5
Andres Maldonado-Martin <andresdmm1@hotmail.com>
2022-03-06 20:53:40 -0500

    Fixing bugs

--- Fighter's Uproar/Assets/GameController.cs
+++ Fighter's Uproar/Assets/GameController.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
         if (Input.GetKey("escape"))
-            Application.Quit();
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
 	}
 }
